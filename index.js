60d8c81c8739efc80363fb7a7eb6535c4987796b
Nguyen Duong <muoi07052001@gmail.com>
2022-11-03 16:11:04 +0700

    Create API Code Base

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-import express from "express";
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import authRoute from './routes/auth.js';
+import usersRoute from './routes/users.js';
+import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
 
 const app = express();
 
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('Connected to MongoDB!');
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected!');
+});
+
+// Middlewares
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
+
 app.listen(4000, () => {
-  console.log("Connected to backend.");
+  connect();
+  console.log('Connected to backend.');
 });

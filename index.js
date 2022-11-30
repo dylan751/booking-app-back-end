9eb14f028662c3f6cd14fa634fcd63f5a3d65f43
Nguyen Duong <muoi07052001@gmail.com>
2022-11-30 14:00:28 +0700

    Deploy

--- index.js
+++ index.js
@@ -13,46 +13,43 @@ const app = express();
 
 dotenv.config();
 
-// const connect = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO);
-//     console.log('Connected to MongoDB!');
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('Connected to MongoDB!');
+  } catch (error) {
+    throw error;
+  }
+};
 
-// mongoose.connection.on('disconnected', () => {
-//   console.log('MongoDB disconnected!');
-// });
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected!');
+});
 
 // Middlewares
 app.use(cors()); // Enable cors
 app.use(cookieParser());
 app.use(express.json());
 
-// app.use('/api/auth', authRoute);
-// app.use('/api/users', usersRoute);
-// app.use('/api/hotels', hotelsRoute);
-// app.use('/api/rooms', roomsRoute);
-// app.use('/api/forms', formsRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
+app.use('/api/forms', formsRoute);
 
-// app.use((err, req, res, next) => {
-//   const errorStatus = err.status || 500;
-//   const errorMessage = err.message || 'Something went wrong!';
-//   return res.status(errorStatus).json({
-//     success: false,
-//     status: errorStatus,
-//     message: errorMessage,
-//     stack: err.stack,
-//   });
-// });
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || 'Something went wrong!';
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 const port = process.env.PORT || 4000;
-app.listen(port, (req, res) => {
-  // connect();
+app.listen(port, () => {
+  connect();
   console.log('Connected to backend.');
-});
-app.use('/', (req, res) => {
-  res.status(200).json('Hello World');
 });

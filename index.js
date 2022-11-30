7a8c937a34d2e1bc43b444d06f4e8f3c1c999117
Nguyen Duong <muoi07052001@gmail.com>
2022-11-30 13:53:45 +0700

    Update vercel.json

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import authRoute from './routes/auth.js';
-import usersRoute from './routes/users.js';
-import hotelsRoute from './routes/hotels.js';
-import roomsRoute from './routes/rooms.js';
-import formsRoute from './routes/forms.js';
+import authRoute from './api/routes/auth.js';
+import usersRoute from './api/routes/users.js';
+import hotelsRoute from './api/routes/hotels.js';
+import roomsRoute from './api/routes/rooms.js';
+import formsRoute from './api/routes/forms.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
